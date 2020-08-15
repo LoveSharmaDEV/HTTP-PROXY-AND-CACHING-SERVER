352bdd7814422df18d9803ca4ba319263fb98090
root <root@localhost.localdomain>
2020-08-15 08:47:57 +0530

    SCODE push

--- proxy.py
+++ proxy.py
@@ -5,15 +5,13 @@ import datetime
 import time
 import json
 
-# global variables
-max_connections = 10
-BUFFER_SIZE = 4096
+MAX_CONN = 10
+RECV_SIZE = 4096
 CACHE_DIR = "./cache"
-MAX_CACHE_BUFFER = 3
-NO_OF_OCC_FOR_CACHE = 2
-
-
+MAX_CACHE_RECORDS = 3
+CACHE_THRESHOLD = 3
 proxy_port = 20000
+
 if not os.path.isdir(CACHE_DIR):
     os.makedirs(CACHE_DIR)
 for file in os.listdir(CACHE_DIR):
@@ -26,14 +24,11 @@ memory = {}
 This fuction deals with keeping track of number of times the resource has been requested.
 '''
 def Cache_Memory(file_requested, client_addr):
-    print(file_requested + '\n')
     file_requested = file_requested.replace("/", "__")
-    print(file_requested + '\n')
     if not file_requested in memory:
         memory[file_requested] = []
     dt = time.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
     memory[file_requested].append({"request_time": dt,"client": json.dumps(client_addr),})
-    print(memory[file_requested])
     
 '''
 This function deals with setting up the caching policy used.
@@ -45,9 +40,9 @@ Caching Policy:
 def Cache_Decision(file_requested):
     try:
         file_request_list = memory[file_requested.replace("/", "__")]
-        if len(file_request_list) < NO_OF_OCC_FOR_CACHE: 
+        if len(file_request_list) < CACHE_THRESHOLD: 
             return False
-        last_resource_request = file_request_list[len(file_request_list)-NO_OF_OCC_FOR_CACHE]["request_time"]
+        last_resource_request = file_request_list[len(file_request_list)-CACHE_THRESHOLD]["request_time"]
         if datetime.datetime.fromtimestamp(time.mktime(last_resource_request)) + datetime.timedelta(minutes=10) >= datetime.datetime.now():
             return True
         else:
@@ -91,17 +86,8 @@ def get_cache_request_info(client_addr, request_info):
 # if cache is full then delete the least recently used cache item
 def freeup_cache(file_requested):
     cache_files = os.listdir(CACHE_DIR)
-    if len(cache_files) < MAX_CACHE_BUFFER:
+    if len(cache_files) < MAX_CACHE_RECORDS:
         return
-  
-    """"
-    last_mtime = min(os.path.getmtime(CACHE_DIR + "/" + file) for file in cache_files)
-    print last_mtime
-    file = [file for file in cache_files if os.path.getmtime(CACHE_DIR + "/" + file) == last_mtime][0]
-    print file
-    os.remove(CACHE_DIR + "/" + file)
-    print file, "removed"
-    """
     last_mtime = min(memory[file][-1]["request_time"] for file in cache_files)
     file_to_del = [file for file in cache_files if memory[file][-1]["request_time"] == last_mtime][0]
     os.remove(CACHE_DIR + "/" + file_to_del)
@@ -113,8 +99,6 @@ This function processes the client data and separates out the essential informat
 '''
 def parse_request_info(client_addr, client_data):
     try:
-        # parse first line like below
-        # http:://127.0.0.1:20020/1.data
 
         lines = client_data.splitlines()
         while lines[len(lines)-1] == '':
@@ -122,7 +106,6 @@ def parse_request_info(client_addr, client_data):
         first_line_tokens = lines[0].split()
         url = first_line_tokens[1]
 
-        # get starting index of IP
         url_pos = url.find("://")
         if url_pos != -1:
             protocol = url[:url_pos]
@@ -188,7 +171,6 @@ def insert_if_modified(request_info):
 # serve get request
 def get_request_handler(client_socket, client_addr, request_info):
     try:
-        #print request_info["client_data"], request_info["do_cache"], request_info["cache_path"], request_info["last_mtime"]
         client_data = request_info["client_data"]
         do_cache = request_info["do_cache"]
         cache_path = request_info["cache_path"]
@@ -199,37 +181,33 @@ def get_request_handler(client_socket, client_addr, request_info):
         server_socket.send(request_info["client_data"])
         
 
-        reply = server_socket.recv(BUFFER_SIZE)
+        reply = server_socket.recv(RECV_SIZE)
         if last_mtime and "304 Not Modified" in str(reply, 'utf-8'):
-            print("returning cached file %s to %s" % (cache_path, str(client_addr)))
+            print("returning cached file "+ cache_path + " to address " + str(client_addr) )
             f = open(cache_path, 'rb')
-            chunk = f.read(BUFFER_SIZE)
+            chunk = f.read(RECV_SIZE)
             while chunk:
                 client_socket.send(chunk)
-                chunk = f.read(BUFFER_SIZE)
+                chunk = f.read(RECV_SIZE)
             f.close()
 
         else:
             if do_cache:
-                print("caching file while serving %s to %s" % (cache_path, str(client_addr)))
+                print("Caching file " + cache_path  + " and sending to " + str(client_addr))
                 freeup_cache(request_info["total_url"])
                 f = open(cache_path, "w+")
-                # print len(reply), reply
                 while len(reply):
                     client_socket.send(reply)
                     f.write(str(reply, 'utf-8'))
-                    reply = server_socket.recv(BUFFER_SIZE)
-                    #print len(reply), reply
+                    reply = server_socket.recv(RECV_SIZE)
                 f.close()
                 client_socket.send(str.encode("\r\n\r\n"))
                 
             else:
-                print("without caching serving %s to %s" % (cache_path, str(client_addr)))
-                #print len(reply), reply
+                print("Not caching " + cache_path  +  " now, contacting server for request from client " + str(client_addr) )
                 while len(reply):
                     client_socket.send(reply)
-                    reply = server_socket.recv(BUFFER_SIZE)
-                    #print len(reply), reply
+                    reply = server_socket.recv(RECV_SIZE)
                 client_socket.send(str.encode("\r\n\r\n"))
 
         server_socket.close()
@@ -250,7 +228,7 @@ def post_request_handler(client_socket, client_addr, request_info):
         server_socket.send(request_info["client_data"])
 
         while True:
-            reply = server_socket.recv(BUFFER_SIZE)
+            reply = server_socket.recv(RECV_SIZE)
             if len(reply):
                 client_socket.send(reply)
             else:
@@ -270,13 +248,12 @@ def post_request_handler(client_socket, client_addr, request_info):
 
 
 
-# A thread function to handle one request
 def request_handler(client_socket, client_addr, client_data):
 
     request_info = parse_request_info(client_addr, client_data)
 
     if not request_info:
-        print("no any request_info")
+        print("There are no connection details")
         client_socket.close()
         return
     elif request_info["method"] == "GET":
@@ -294,20 +271,18 @@ def request_handler(client_socket, client_addr, client_data):
 
 
 
-# This funciton initializes socket and starts listening.
-# When connection request is made, a new thread is created to serve the request
 def proxy_handler():
 
     try:
         proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         proxy_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         proxy_socket.bind(('', proxy_port))
-        proxy_socket.listen(max_connections)
-
-        print("Serving proxy on %s port %s ..." % (str(proxy_socket.getsockname()[0]),str(proxy_socket.getsockname()[1])))
-
+        proxy_socket.listen(MAX_CONN)
+        
+        print("Startig proxy on "+ str(proxy_socket.getsockname()[0]) + " port " + str(proxy_socket.getsockname()[1])) 
+        
     except Exception as e:
-        print("Error in starting proxy server ...")
+        print("Some Error Occured ...")
         print(e)
         proxy_socket.close()
         raise SystemExit
@@ -317,7 +292,7 @@ def proxy_handler():
     while True:
         try:
             client_socket, client_addr = proxy_socket.accept()
-            client_data = client_socket.recv(BUFFER_SIZE)
+            client_data = client_socket.recv(RECV_SIZE)
             print (str(client_addr[0] + ' '+ str(datetime.datetime.now()) + ' '+ str(client_data , 'utf-8')))
 
             _thread.start_new_thread(request_handler, (client_socket,client_addr,str(client_data , 'utf-8')))
